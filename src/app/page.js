f2bec28d32360ddbfb2f1e94227ee9c874a0d1b3
Jason Wilson <info@jasonleewilson.com>
2024-05-05 20:46:18 -0500

    added bunny

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,18 @@ export const runtime = "edge";
 export default function Home() {
   return (
     <main className='grid h-screen place-items-center'>
-      <h1>charlee.io</h1>
+      <div className='text-center'>
+        <Image
+          src='./bunny.svg'
+          alt='profile'
+          width='0'
+          height='0'
+          sizes='100vw'
+          className='inline-block w-40 h-40'
+          priority={true}
+        />
+        <h1>charlee.io</h1>
+      </div>
     </main>
   );
 }
