2d95f59e37c3ad3f70f2fd0070b6a000122b2f5b
Jason Wilson <robotici@yahoo.com>
2026-01-26 22:13:09 -0600

    updated footer year

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,13 +2,17 @@ import React from "react";
 const companyName = "Charlee";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div>
-      <footer className='bg-white text-center lg:text-left'>
-        <div className='p-4 text-center text-black text-surface'>
-          © 2024 Copyright <a href='#'>{companyName}.io</a>
+      <footer className="bg-white text-center lg:text-left">
+        <div className="p-4 text-center text-black text-surface">
+          © {currentYear} Copyright{" "}
+          <a href="#">{companyName}.io</a>
         </div>
       </footer>
     </div>
   );
 }
+
